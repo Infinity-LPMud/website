81f4b9b2016c69af425b223f999265ede5ead17a
Steve Giralt <88471484+SteveGiralt@users.noreply.github.com>
2024-01-11 13:55:37 -0700

    use React to resize parchment

---
+++ src/components/ParchmentContainer.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const ParchmentContainer = (props: { children: React.ReactNode }) => {
+  const { children } = props;
+  const childRef = React.useRef<HTMLDivElement>(null);
+  const [parchmentHeight, setParchmentHeight] = React.useState<number>(5000);
+  const [windowSize, setWindowSize] = React.useState<number>();
+
+  const updateParchmentSize = () => {
+    if (childRef.current === null) return;
+    setParchmentHeight(childRef.current.getBoundingClientRect().height + 100);
+  };
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      updateParchmentSize();
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="relative flex flex-col w-3/4 h-auto mx-auto p-16">
+        <div id="parchment" style={{ height: `${parchmentHeight}px` }}></div>
+        <div ref={childRef}>{children}</div>
+      </div>
+
+      <svg>
+        <filter id="wavy2">
+          <feTurbulence
+            x="0"
+            y="0"
+            baseFrequency="0.02"
+            numOctaves="5"
+            seed="1"
+          ></feTurbulence>
+          <feDisplacementMap in="SourceGraphic" scale="20"></feDisplacementMap>
+        </filter>
+      </svg>
+    </>
+  );
+};
+
+export default ParchmentContainer;
