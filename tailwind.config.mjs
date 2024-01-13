fc739198a79dc11f6d4ae79a763a45f498b07a73
Steve Giralt <88471484+SteveGiralt@users.noreply.github.com>
2024-01-13 14:37:56 -0700

    add history and qualstar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,7 +12,9 @@ export default {
         caveat: ["Caveat", "cursive"],
       },
       backgroundImage: {
-        "paper-bg": 'url("/assets/paper-bg2.jpg")',
+        // "paper-bg": 'url("/assets/paper-bg2.jpg")',
+        "paper-bg":
+          'linear-gradient(rgba(200, 200, 200, .5), rgba(200, 200, 200, .5)), url("/assets/paper-bg2.jpg")',
       },
     },
   },
